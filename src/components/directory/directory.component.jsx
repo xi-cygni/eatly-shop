def7b7c7d44b5a803b35506c20aca304af8dcd37
Agnieszka Gawron <gawron.agnieszka.m@gmail.com>
2021-02-27 17:07:16 +0100

    broken page into smaller components, finished styling menu-item

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: [
+                {
+                    title: 'COFFEE',
+                    imageUrl: 'https://images.unsplash.com/photo-1593536673055-14892cdffac7?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1349&q=80',
+                    id: 1,
+                    linkUrl: 'shop/coffee'
+                },
+                {
+                    title: 'SWEETS',
+                    imageUrl: 'https://images.unsplash.com/photo-1585198330882-f32e816d0320?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+                    id: 2,
+                    linkUrl:   'shop/sweets'
+                },
+                {
+                    title: 'FROMAGGI',
+                    imageUrl: 'https://images.unsplash.com/photo-1452195100486-9cc805987862?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+                    id: 3,
+                    linkUrl: 'shop/fromaggi'
+                },
+                {
+                    title: 'WINE & SPIRITS',
+                    imageUrl: 'https://images.unsplash.com/photo-1516594915697-87eb3b1c14ea?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
+                    size: 'large',
+                    id: 4,
+                    linkUrl: 'shop/spirits'
+                },
+                {
+                    title: 'ACCESSORIES',
+                    imageUrl: 'https://images.unsplash.com/photo-1442512595331-e89e73853f31?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
+                    size: 'large',
+                    id: 5,
+                    linkUrl: 'shop/accessories'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    // destructuring again 
+                    this.state.sections.map(({title, imageUrl, size, id}) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Directory;
