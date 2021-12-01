1700748ad96acbf5c127ce4fe03a52cacb52e7af
Agnieszka Gawron <gawron.agnieszka.m@gmail.com>
2021-12-01 23:11:10 +0100

    Added sign in and form

---
+++ src/components/sign-in-sing-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignIn from '../sign-in/sign-in.component';
+import './sign-in-sign-up.styles.scss';
+
+const SignInSignUpPage = () => (
+    <div className='sign-in-sign-up'>
+        <SignIn />
+    </div>
+);
+
+export default SignInSignUpPage;
